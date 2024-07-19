ad937f5b2c0d89d695d68f9a4f7ce8eb8590e69b
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2024-07-19 02:23:56 +0200

    doc fixes

--- EXILED/Exiled.API/Features/Server.cs
+++ EXILED/Exiled.API/Features/Server.cs
@@ -218,8 +218,8 @@ namespace Exiled.API.Features
         /// <summary>
         /// Shutdowns the server, disconnects all players.
         /// </summary>
-        /// <param name="quit">idk ???.</param>
-        /// <param name="suppressShutdownBroadcast"> supress the shutdown broadcast.</param>
+        /// <param name="quit">Whether to quit the application.</param>
+        /// <param name="suppressShutdownBroadcast">Whether to supress the shutdown broadcast.</param>
         /// <seealso cref="ShutdownRedirect(ushort)"/>
         public static void Shutdown(bool quit, bool suppressShutdownBroadcast = false) => global::Shutdown.Quit(quit, suppressShutdownBroadcast);
 
